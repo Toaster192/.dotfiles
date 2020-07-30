6de9cafe94eb4b2161e3e621000ce96001527eb9
Ondrej Kinst <toastercz@gmail.com>
2020-07-30 10:32:03 +0200

    Awesome: Fix default layout warning

--- .config/awesome/config/tags.lua
+++ .config/awesome/config/tags.lua
@@ -73,30 +73,28 @@ function _config.init()
     --     layout = cond,
     -- }
 
-    awful.layout.layouts = {
+    awful.layout.remove_default_layout(awful.layout.suit.max.fullscreen)
+    awful.layout.remove_default_layout(awful.layout.suit.tile.bottom)
+    awful.layout.remove_default_layout(awful.layout.suit.tile.top)
+    awful.layout.remove_default_layout(awful.layout.suit.corner.nw)
+    awful.layout.remove_default_layout(awful.layout.suit.corner.ne)
+    awful.layout.remove_default_layout(awful.layout.suit.corner.sw)
+    awful.layout.remove_default_layout(awful.layout.suit.corner.se)
+    awful.layout.remove_default_layout(awful.layout.suit.fair.horizontal)
+    awful.layout.remove_default_layout(awful.layout.suit.spiral)
+    awful.layout.remove_default_layout(awful.layout.suit.spiral.dwindle)
+
+    awful.layout.append_default_layouts({
         awful.layout.suit.max,
-        --awful.layout.suit.max.fullscreen,
-        awful.layout.suit.tile,
-        awful.layout.suit.tile.left,
-        --awful.layout.suit.tile.bottom,
-        --awful.layout.suit.tile.top,
         awful.layout.suit.fair,
-        --awful.layout.suit.fair.horizontal,
-        --awful.layout.suit.spiral,
-        --awful.layout.suit.spiral.dwindle,
         awful.layout.suit.magnifier,
-        --awful.layout.suit.corner.nw,
-        --awful.layout.suit.corner.ne,
-        --awful.layout.suit.corner.sw,
-        --awful.layout.suit.corner.se,
         --lain.layout.cascade,
         lain.layout.cascade.tile,
         lain.layout.centerwork,
         --lain.layout.centerwork.horizontal,
         --lain.layout.termfair,
         --lain.layout.termfair.center,
-        awful.layout.suit.floating,
-    }
+    })
 
     _config.layouts = {
         awful.layout.suit.max,
